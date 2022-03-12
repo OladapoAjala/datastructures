15daed4a55a09d1b9d89aeeed77f8083dcd5c067
Oladapo AJala <oladapoajala.e@gmail.com>
2022-03-12 12:16:47 +0100

    ADD: implemented node package

---
+++ node/node.go
@@ -0,0 +1,27 @@
+package node
+
+/*
+Abstract Data Types:
+1. List
+2. Queue
+3. Stack
+4. Map
+5.
+
+Possible implementations
+1. List: LinkedList, Dynamic Array
+2. Queue: LinkedList, Dynamic Array
+3. Stack: LinkedList, Dynamic Array
+4. Map: Trees, Hash Map, Hash Table
+
+*/
+type Node struct {
+	Data  interface{}
+	Index int32
+	Prev  *Node
+	Next  *Node
+}
+
+func NewNode() *Node {
+	return new(Node)
+}
