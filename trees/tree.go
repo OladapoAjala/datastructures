3acd29feb05dc9ef3e1fab0a8a8283b642f312f6
Dapo Ajala <dapo@sephia.io>
2023-10-07 16:19:25 +0200

    WIP: base tree interface

--- trees/tree.go
+++ trees/tree.go
@@ -1,4 +1,20 @@
 package trees
 
-type ITrees struct {
+import (
+	"github.com/OladapoAjala/datastructures/sequences"
+	"github.com/OladapoAjala/datastructures/sets"
+	"github.com/OladapoAjala/datastructures/trees/node"
+	"golang.org/x/exp/constraints"
+)
+
+type ITrees[T constraints.Ordered] interface {
+	SubTreeFirst(*node.Node[T]) //leftmost node
+	Successor(*node.Node[T])    //immediate element after current element in in-order traversal (there are two cases)
+	Predecessor(*node.Node[T])
+	SubTreeInsertAfter(*node.Node[T], *node.Node[T])  // replace successor with new node
+	SubTreeInsertBefore(*node.Node[T], *node.Node[T]) // replace predecessor with new node
+	Sequence() *sequences.Sequencer[T]
+	Set() *sets.Seter[T, any]
+	Subtree(*node.Node[T], int32)
+	SubtreeAugmentation()
 }
