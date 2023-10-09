bcde4b8de4c8afe84d5ce98133c9582bc1382e80
Dapo Ajala <dapo@sephia.io>
2023-10-09 23:42:14 +0200

    WIP: proposed interface

--- trees/tree.go
+++ trees/tree.go
@@ -2,19 +2,28 @@ package trees
 
 import (
 	"github.com/OladapoAjala/datastructures/sequences"
-	"github.com/OladapoAjala/datastructures/sets"
 	"github.com/OladapoAjala/datastructures/trees/node"
-	"golang.org/x/exp/constraints"
 )
 
-type ITrees[T constraints.Ordered] interface {
-	SubTreeFirst(*node.Node[T]) //leftmost node
-	Successor(*node.Node[T])    //immediate element after current element in in-order traversal (there are two cases)
-	Predecessor(*node.Node[T])
+type ITrees[T any] interface {
+	Insert(T) error
+	Remove(T) error
+
+	Successor(*node.Node[T]) *node.Node[T] //immediate element after current element in in-order traversal (there are two cases)
+	Predecessor(*node.Node[T]) *node.Node[T]
+
+	SubTreeFirst(*node.Node[T])                       //leftmost node
 	SubTreeInsertAfter(*node.Node[T], *node.Node[T])  // replace successor with new node
 	SubTreeInsertBefore(*node.Node[T], *node.Node[T]) // replace predecessor with new node
+
 	Sequence() *sequences.Sequencer[T]
-	Set() *sets.Seter[T, any]
-	Subtree(*node.Node[T], int32)
+	// Set() *sets.Seter[T, any]
+
+	// Subtree(*node.Node[T], int32) (*node.Node[T], error)
 	SubtreeAugmentation()
+	GetNodeHeight(T) int32
+	GetNodeDepth(T) int32
+
+	// Order
+	TraversalOrder(*node.Node[T]) ([]T, error)
 }
