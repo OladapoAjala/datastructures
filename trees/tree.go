d37f0d1957a7a8f63d411184da7adc65fb348dbb
Dapo Ajala <dapo@sephia.io>
2023-10-29 04:52:17 +0100

    latest interface

--- trees/tree.go
+++ trees/tree.go
@@ -5,8 +5,6 @@ import (
 )
 
 type ITrees[T comparable] interface {
-	InsertAfter(*node.Node[T], T) error
-	InsertBefore(*node.Node[T], T) error
 	Successor(*node.Node[T]) (*node.Node[T], error)
 	Predecessor(*node.Node[T]) (*node.Node[T], error)
 	SubTreeFirst(*node.Node[T]) (*node.Node[T], error)
@@ -14,16 +12,11 @@ type ITrees[T comparable] interface {
 	TraversalOrder(*node.Node[T]) ([]T, error)
 	PreOrderTraversal(*node.Node[T]) ([]T, error)
 	PostOrderTraversal(*node.Node[T]) ([]T, error)
-
-	// Subtree(*node.Node[T], int32) (*node.Node[T], error)
+	GetSize() int32
+	GetHeight() int32
 	// SubtreeAugmentation()
 	// GetNodeHeight(*node.Node[T]) int32
 	// GetNodeDepth(*node.Node[T]) int32
-
-	// Insert(T) error
 	// Sequence() *sequences.Sequencer[T]
 	// Set() *sets.Seter[T, any]
-
-	GetSize() int32
-	GetHeight() int32
 }
