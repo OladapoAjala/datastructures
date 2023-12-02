92285f5f82118ed1d5c3fe06c90c8ae99d65dea5
Dapo Ajala <dapo@sephia.io>
2023-12-02 21:56:15 +0100

    more tests

--- graph/vertex/vertex_test.go
+++ graph/vertex/vertex_test.go
@@ -34,4 +34,45 @@ func Test_Vertex(t *testing.T) {
 	is.True(c.HasEdge(f.GetState()))
 	is.True(e.HasEdge(a.GetState()))
 	is.True(e.HasEdge(h.GetState()))
+
+	is.False(a.HasEdge(d.GetState()))
+	is.False(a.HasEdge(e.GetState()))
+	is.False(a.HasEdge(f.GetState()))
+	is.False(a.HasEdge(h.GetState()))
+
+	is.False(b.HasEdge(a.GetState()))
+	is.False(b.HasEdge(c.GetState()))
+	is.False(b.HasEdge(f.GetState()))
+	is.False(b.HasEdge(h.GetState()))
+
+	is.False(c.HasEdge(a.GetState()))
+	is.False(c.HasEdge(b.GetState()))
+	is.False(c.HasEdge(d.GetState()))
+	is.False(c.HasEdge(h.GetState()))
+
+	is.False(d.HasEdge(a.GetState()))
+	is.False(d.HasEdge(b.GetState()))
+	is.False(d.HasEdge(c.GetState()))
+	is.False(d.HasEdge(e.GetState()))
+	is.False(d.HasEdge(f.GetState()))
+	is.False(d.HasEdge(h.GetState()))
+
+	is.False(e.HasEdge(b.GetState()))
+	is.False(e.HasEdge(c.GetState()))
+	is.False(e.HasEdge(d.GetState()))
+	is.False(e.HasEdge(f.GetState()))
+
+	is.False(f.HasEdge(a.GetState()))
+	is.False(f.HasEdge(b.GetState()))
+	is.False(f.HasEdge(c.GetState()))
+	is.False(f.HasEdge(d.GetState()))
+	is.False(f.HasEdge(e.GetState()))
+	is.False(f.HasEdge(h.GetState()))
+
+	is.False(h.HasEdge(a.GetState()))
+	is.False(h.HasEdge(b.GetState()))
+	is.False(h.HasEdge(c.GetState()))
+	is.False(h.HasEdge(d.GetState()))
+	is.False(h.HasEdge(e.GetState()))
+	is.False(h.HasEdge(f.GetState()))
 }
